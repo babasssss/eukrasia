db34b1362fcf9813ad0e50ac1564b70028da8fc9
SOREAU BASTIEN <80333079+babasssss@users.noreply.github.com>
2024-10-24 19:42:10 +0200

    Initial commit

---
+++ app/orgs/[orgSlug]/(navigation)/settings/layout.tsx
@@ -0,0 +1,42 @@
+import {
+  Layout,
+  LayoutContent,
+  LayoutHeader,
+  LayoutTitle,
+} from "@/features/page/layout";
+import { createSearchParamsMessageUrl } from "@/features/searchparams-message/createSearchParamsMessageUrl";
+import { combineWithParentMetadata } from "@/lib/metadata";
+import { getServerUrl } from "@/lib/server-url";
+import { SiteConfig } from "@/site-config";
+import type { LayoutParams } from "@/types/next";
+import { redirect } from "next/navigation";
+
+export const generateMetadata = combineWithParentMetadata({
+  title: "Settings",
+  description: "Manage your organization settings.",
+});
+
+export default async function RouteLayout(
+  props: LayoutParams<{ productId: string; orgSlug: string }>,
+) {
+  if (SiteConfig.features.enableSingleMemberOrg) {
+    redirect(
+      createSearchParamsMessageUrl(
+        `${getServerUrl()}org/${props.params.orgSlug}`,
+        {
+          type: "message",
+          message: "You need to update your account settings.",
+        },
+      ),
+    );
+  }
+
+  return (
+    <Layout>
+      <LayoutHeader>
+        <LayoutTitle>Organization</LayoutTitle>
+      </LayoutHeader>
+      <LayoutContent>{props.children}</LayoutContent>
+    </Layout>
+  );
+}
